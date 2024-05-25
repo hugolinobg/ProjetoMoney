4b5b24219f669f474c841c9094c8beb00906996f
Hugolino B. Guimarães <hugolino2609@gmail.com>
2024-05-25 12:15:37 -0300

    Update Home.jsx

--- money-fontEnd/src/pages/Home/Home.jsx
+++ money-fontEnd/src/pages/Home/Home.jsx
@@ -1,12 +1,6 @@
 import './Home.css'
-
-<<<<<<< HEAD
 import Sidebar from '../../components/Sidebar/Sidebar'
 import Navbar from '../../components/Navbar/Navbar'
-=======
-import Sidebar from "../../components/Sidebar/Sidebar"
-import Navbar from "../../components/Navbar/Navbar"
->>>>>>> 41bad6e195912c1de11cd655db7142c574879a7d
 
 const Home = () => {
   return (
